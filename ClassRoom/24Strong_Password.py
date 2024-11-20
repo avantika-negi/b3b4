9870bdffad7b224e5fe3632dfde0b428ad1f3d92
avantika-negi <Avantika.16710@stu.upes.ac.in>
2024-11-20 06:04:00 +0530

    20-11-2024

--- ClassRoom/24Strong_Password.py
+++ ClassRoom/24Strong_Password.py
@@ -1,3 +1,16 @@
 # WAP THAT CHECKS IF A PASSWORD IS STRONG.
 
- 
+import re
+
+def is_strong_password(password):
+    pattern=r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@\( !%?&])[A-Za-z\d@ \)!%?&]{8,}$'
+    if re.match(pattern, password):
+        return True
+    else:
+        return False
+
+
+if is_strong_password("Password123@"):
+    print("Strong pass")
+else:
+    print("Not strong pass")
